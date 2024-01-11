e5ce4b87e96d124757fb38ca44d47b11e718ee31
ckechios <62009820+ckechios@users.noreply.github.com>
2024-01-11 12:57:39 +0000

    Created server

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http') 
+const port = 8080 
+
+// Create a server object: 
+const server = http.createServer(function (req, res) { 
+
+	// Write a response to the client 
+	res.write('<H1>Test successfull</H1>') 
+
+	// End the response 
+	res.end() 
+}) 
+
+// Set up our server so it will listen on the port 
+server.listen(port, function (error) { 
+
+	// Checking any error occur while listening on port 
+	if (error) { 
+		console.log('Error', error); 
+	} 
+	// Else sent message of listening 
+	else { 
+		console.log('Server is listening on port' + port); 
+	} 
+})
